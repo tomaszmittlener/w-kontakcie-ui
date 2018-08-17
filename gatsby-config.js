9111fb2a8eecd23d7446efd058278393b73fa0d7
Tomasz Mittlener <t.mittlener@gmail.com>
2018-08-17 10:08:40 +0200

    feat: add rss and revert pathPrefixing

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,22 @@
 const config = require('./data/SiteConfig')
+const urljoin = require('url-join')
 
 module.exports = {
+  pathPrefix: config.pathPrefix,
   siteMetadata: {
-    siteUrl: config.siteUrl,
+    siteUrl: urljoin(config.siteUrl, config.pathPrefix),
+    rssMetadata: {
+      site_url: urljoin(config.siteUrl, config.pathPrefix),
+      feed_url: urljoin(config.siteUrl, config.pathPrefix, config.siteRss),
+      title: config.siteTitle,
+      description: config.siteDescription,
+      image_url: `${urljoin(
+        config.siteUrl,
+        config.pathPrefix,
+      )}/logos/logo-512.png`,
+      author: config.userName,
+      copyright: config.copyright,
+    },
   },
   plugins: [
     'gatsby-transformer-sharp',
@@ -68,7 +82,7 @@ module.exports = {
         name: config.siteTitle,
         short_name: config.siteTitleShort,
         description: config.siteDescription,
-        start_url: config.siteUrl,
+        start_url: config.pathPrefix,
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
         display: 'minimal-ui',
@@ -87,5 +101,78 @@ module.exports = {
       },
     },
     'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-plugin-feed',
+      options: {
+        setup(ref) {
+          const ret = ref.query.site.siteMetadata.rssMetadata
+          ret.allMarkdownRemark = ref.query.allMarkdownRemark
+          ret.generator = 'W Kontakcie'
+          return ret
+        },
+        query: `
+        {
+          site {
+            siteMetadata {
+              rssMetadata {
+                site_url
+                feed_url
+                title
+                description
+                image_url
+                author
+                copyright
+              }
+            }
+          }
+        }
+      `,
+        feeds: [
+          {
+            serialize(ctx) {
+              const {rssMetadata} = ctx.query.site.siteMetadata
+              return ctx.query.allMarkdownRemark.edges.map(edge => ({
+                categories: edge.node.frontmatter.tags,
+                date: edge.node.fields.date,
+                title: edge.node.frontmatter.title,
+                description: edge.node.excerpt,
+                author: rssMetadata.author,
+                url: rssMetadata.site_url + edge.node.fields.slug,
+                guid: rssMetadata.site_url + edge.node.fields.slug,
+                custom_elements: [{'content:encoded': edge.node.html}],
+              }))
+            },
+            query: `
+            {
+              allMarkdownRemark(
+                limit: 1000,
+                sort: { order: DESC, fields: [fields___date] },
+              ) {
+                edges {
+                  node {
+                    excerpt
+                    html
+                    timeToRead
+                    fields {
+                      slug
+                      date
+                    }
+                    frontmatter {
+                      title
+                      cover
+                      date
+                      category
+                      tags
+                    }
+                  }
+                }
+              }
+            }
+          `,
+            output: config.siteRss,
+          },
+        ],
+      },
+    },
   ],
 }
