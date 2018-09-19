aba76b73b85a17ff336d5e54155c5fafb90e893d
Tomasz Mittlener <t.mittlener@gmail.com>
2018-09-19 18:26:05 +0200

    feat: remove particles

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import {graphql} from 'gatsby'
-import Particles from 'react-particles-js'
 
 import {compose} from 'src/utils'
 import {withLocales, withLocalesContextProvider} from 'src/context'
@@ -25,16 +24,6 @@ import {
 } from 'src/utils/PropTypes'
 
 import config from '../../data/SiteConfig'
-import particlesConfig from '../../data/particlesjs-config'
-
-const StyledParticles = styled(Particles)`
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  z-index: ${({theme: {layers}}) => layers.bottom};
-`
 
 // about me
 const AboutMeContainer = styled.div`
@@ -66,7 +55,6 @@ class Index extends React.Component {
     } = this.props
     return (
       <Layout location={location} withTopPadding hideLogo>
-        <StyledParticles params={particlesConfig} />
         <Helmet title={config.siteTitle} />
         <SEO />
         <PageSection>
